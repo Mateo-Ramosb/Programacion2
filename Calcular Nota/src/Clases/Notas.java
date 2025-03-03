c5ac48ab0f140b0cfd11d2c9385f11d6745939dd
Mateo-Ramosb <mateo.ramosbaena@unitecnar.edu.co>
2025-03-03 08:45:56 -0500

    Add files via upload

---
+++ Calcular Nota/src/Clases/Notas.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Sala de Sistemas
+ */
+public class Notas {
+    private double n1;
+    private double n2;
+    private double n3;
+
+    /**
+     * @return the n1
+     */
+    public double getN1() {
+        return n1;
+    }
+
+    /**
+     * @param n1 the n1 to set
+     */
+    public void setN1(double n1) {
+        this.n1 = n1;
+    }
+
+    /**
+     * @return the n2
+     */
+    public double getN2() {
+        return n2;
+    }
+
+    /**
+     * @param n2 the n2 to set
+     */
+    public void setN2(double n2) {
+        this.n2 = n2;
+    }
+
+    /**
+     * @return the n3
+     */
+    public double getN3() {
+        return n3;
+    }
+
+    /**
+     * @param n3 the n3 to set
+     */
+    public void setN3(double n3) {
+        this.n3 = n3;
+    }
+    
+    
+   
+    
+    
+    
+}
---
+++ Calcular Nota/src/calcular/nota/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calcular.nota;
+
+import Formulario.FrmNotas;
+
+/**
+ *
+ * @author Sala de Sistemas
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        FrmNotas f = new FrmNotas();
+        f.setVisible(true);
+    }
+    
+}
